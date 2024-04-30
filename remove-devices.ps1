4df7db75587f10d90e084ec22286f1c34556cf4d
slipsknuten <gurra.bergqvist@gmail.com>
2024-04-30 14:20:41 +0200

    asd

--- remove-devices.ps1
+++ remove-devices.ps1
@@ -1,4 +1,4 @@
-$deviceName="HP E24m G4 USB Audio", "HP E243d Audio"
+$deviceName="HP E24m G4 USB Audio", "HP E243d Audio", "HP 27m G4 USB Audio"
 foreach($device in $deviceName){
   foreach ($dev in (Get-PnpDevice | Where-Object{$_.Name -eq $device})) {
     &"pnputil" /disable-device $dev.InstanceId 
