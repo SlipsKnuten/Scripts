442c43d3b975e738c15bcbd6d14793fcf01f5baf
slipsknuten <gurra.bergqvist@gmail.com>
2024-05-14 14:22:07 +0200

    re order modules

--- get_members_of_sharedMailbox.ps1
+++ get_members_of_sharedMailbox.ps1
@@ -1,11 +1,11 @@
 #Dessa behöver hämtas och installeras innan man kör scriptet
 
-#Connect-ExchangeOnline
-#Get-Module ImportExcel -ListAvailable | Import-Module -Force -Verbose
-
 #Install-Module ImportExcel -AllowClobber -Force
 #Import-Module ExchangeOnlineManagement
 #Install-Module ExchangeOnlineManagement
+
+#Get-Module ImportExcel -ListAvailable | Import-Module -Force -Verbose
+#Connect-ExchangeOnline
 
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
